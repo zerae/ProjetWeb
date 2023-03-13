24b3a7e35f56f6b4bf89f1917e4222837503f8e2
zerae <119691142+zerae@users.noreply.github.com>
2023-03-13 01:25:14 +0100

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'pmso-91806',
+    appId: '1:520303504976:web:da4fc826917f13d3802b55',
+    storageBucket: 'pmso-91806.appspot.com',
+    apiKey: 'AIzaSyAAR7L4CExN198U9bjo8Ljx7pD5q4cLYoM',
+    authDomain: 'pmso-91806.firebaseapp.com',
+    messagingSenderId: '520303504976',
+    measurementId: 'G-Z54LWKXNXB',
+  },
+    production: false
+};
